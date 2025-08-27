97722a433d0b9e7acc32c2d752e123c4f9c81a08
grisha <grisha.endorphone@gmail.com>
2025-08-28 00:51:07 +0300

    fix favorite page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 import "./index.scss";
 
 import App from "./App";
 
+const CustomRouter =
+  process.env.NODE_ENV === "production" ? HashRouter : Router;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router basename="/react-sneakers">
+  <CustomRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </Router>
+  </CustomRouter>
 );
