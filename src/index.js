02ccd687c007b51830efbb63111ef72a6698d792
grisha <grisha.endorphone@gmail.com>
2025-08-28 09:08:34 +0300

    delete HashRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, HashRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.scss";
 
 import App from "./App";
 
-const CustomRouter =
-  process.env.NODE_ENV === "production" ? HashRouter : Router;
+// const CustomRouter =
+//   process.env.NODE_ENV === "production" ? HashRouter : Router;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <CustomRouter basename={process.env.PUBLIC_URL}>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
-  </CustomRouter>
+  </Router>
 );
